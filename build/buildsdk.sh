ccd3937b7a78e0e7869bc32c81e57e11e60be8e2
TAiGA <52752725+metarutaiga@users.noreply.github.com>
2023-03-01 14:57:27 +0800

    Update clang

--- build/buildsdk.sh
+++ build/buildsdk.sh
@@ -10,7 +10,7 @@ fi
 
 cd hello_world
 if [ ! -d "build" ]; then
-  idf.py set-target esp32-c3
+  idf.py -D IDF_TOOLCHAIN=clang set-target esp32-c3
 fi
 idf.py -D IDF_TOOLCHAIN=clang menuconfig
 if [ ! -d "../esp-clang/riscv32-esp-elf/include/c++" ]; then
